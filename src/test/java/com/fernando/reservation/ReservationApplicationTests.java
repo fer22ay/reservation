fc9b75e0baa2ffb6d2b55e6e93976888e0fb6def
Fernando Ambrosio <fer22ay@gmail.com>
2020-07-10 11:25:25 -0600

    Primer commit de la aplicacion en desarrollo

---
+++ src/test/java/com/fernando/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
